b354e126393f5e9a6528d04c0de3b2ba1950bde9
audrhks29 <audrhks29@gmail.com>
2024-04-13 00:46:18 +0900

    test

--- backEnd/src/routes/salary.ts
+++ backEnd/src/routes/salary.ts
@@ -8,19 +8,24 @@ module.exports = function (app: any, Salary: any) {
     }
   });
 
-  // app.get("/salary/:id/:year/:month", async (req: any, res: any) => {
-  //   try {
-  //     const { id, year, month } = req.params;
-  //     const data = await Salary.find({});
-  //     const salaryData = await data
-  //       .find((item: SalaryDataTypes) => item.employee_number === id)
-  //       ?.data.find((item: { year: number }) => item.year === Number(year))
-  //       ?.salary.find(
-  //         (item: { month: number }) => item.month === Number(month)
-  //       );
-  //     res.json(salaryData);
-  //   } catch (err: any) {
-  //     res.status(500).json({ error: err.message });
-  //   }
-  // });
+  app.get("/salary/:id/:year", async (req: any, res: any) => {
+    try {
+      const { id, year, month } = req.params;
+      const data = await Salary.find({});
+      const salaryData = data.find(
+        (item: SalaryDataTypes) => item.employee_number === id
+      );
+      // const salaryDate2 = salaryData.data.filter(
+      //   (item: any) => item.year === 2024
+      // );
+      // res.json(salaryData.data);
+      // res.send(req.params);
+      const year2024Data = salaryData.find(
+        (entry: { data: any; year: any }) => entry.data.year === year
+      );
+      res.json(year2024Data);
+    } catch (err: any) {
+      res.status(500).json({ error: err.message });
+    }
+  });
 };
