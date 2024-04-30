838c0b579274b691f58138cd1681f742b710195b
audrhks29 <audrhks29@gmail.com>
2024-05-01 06:03:52 +0900

    Feat: create document page

---
+++ document/src/App.tsx
@@ -0,0 +1,23 @@
+/** @jsxImportSource @emotion/react */
+
+function App() {
+  return (
+    <div
+      css={{
+        borderRadius: "6px",
+        border: "1px solid rgba(27, 31, 36, 0.15)",
+        backgroundColor: "rgb(246, 248, 250)",
+        color: "rgb(36, 41, 47)",
+        fontFamily: "-apple-system, BlinkMacSystemFont, sans-serif",
+        fontWeight: "600",
+        fontSize: "14px",
+        padding: "5px 16px",
+        textAlign: "center",
+      }}
+    >
+      소개페이지 제작 예정
+    </div>
+  );
+}
+
+export default App;
